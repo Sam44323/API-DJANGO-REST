d5a15255b8a55e72231a08e09cf652fa9cc409ce
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-30 23:46:57 +0530

    update: updated the template directory structure

--- basicAPI/demo_1/views.py
+++ basicAPI/demo_1/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 def index(request):
     my_dict = {'name': "This is from views.py !"}
-    return render(request, 'index.html', context=my_dict)
+    return render(request, 'demo_1/index.html', context=my_dict)
