1a66ce38b0c477b2af904ae2b7219441b206bfe3
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-30 22:23:21 +0530

    add: added an index mapping for the views for index

--- basicAPI/basicAPI/urls.py
+++ basicAPI/basicAPI/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from demo_1 import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
