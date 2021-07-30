6e3344fe4cb6712da044d0dd902fa72056d8064c
Suranjan <suranjanbiswas2002@gmail.com>
2021-07-30 22:47:03 +0530

    fix: fixed an url routing bug for the main urls.py file

--- basicAPI/basicAPI/urls.py
+++ basicAPI/basicAPI/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    path('demo_1', include('demo_1.urls')),
+    path('demo_1/', include('demo_1.urls')),
     path('admin/', admin.site.urls),
 ]
