44dd356d1bdf5a2faa058598a41ec7589952e80a
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-04 23:29:45 +0530

    update: updated the views.py file

--- basicAPI/demo_1/views.py
+++ basicAPI/demo_1/views.py
@@ -6,6 +6,5 @@ from demo_1.models import AccessRecord, Topic, Webpage
 
 def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
-    print(webpages_list)
     date_dict = {'access_records': webpages_list}
     return render(request, 'demo_1/index.html', context=date_dict)
