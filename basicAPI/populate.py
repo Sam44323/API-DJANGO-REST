c2f76e1802e5939a5a722e0a90fbe1830bcb5e90
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-04 22:15:44 +0530

    add: added the population script

--- basicAPI/populate.py
+++ basicAPI/populate.py
@@ -1,14 +1,7 @@
-from demo_1.models import AccessRecord, Topic, Webpage
 from faker import Faker
 import random
 import django
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicAPI.settings')
-
-# configuring django for script
-
-django.setup()
-
 
 fakegen = Faker()
 topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
@@ -18,3 +11,32 @@ def add_topic():
     t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
     t.save()
     return t
+
+
+def populate(N=5):
+
+    for entry in range(N):
+        # get the topic for entry
+        top = add_topic()
+
+        # Create the fake data for entry
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
+        fake_name = fakegen.company()
+
+        # Create the new webpage entry
+        webpg = Webpage.objects.get_or_create(
+            topic=top, url=fake_url, name=fake_name)[0]
+
+        # Create a fake access record for the webpage
+        acc_rec = AccessRecord.objects.get_or_create(
+            name=webpg, date=fake_date)[0]
+
+
+if __name__ == '__main__':
+    print("Populating the script!")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'basicAPI.settings')
+    django.setup()
+    from demo_1.models import AccessRecord, Topic, Webpage
+    populate(20)
+    print("Population is complete!")
