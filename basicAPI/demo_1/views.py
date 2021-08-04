1f4cd19daaa4e58232a19fdf90ca970233015e8e
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-04 22:48:42 +0530

    update: added model queries to the views

--- basicAPI/demo_1/views.py
+++ basicAPI/demo_1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from demo_1.models import AccessRecord, Topic, Webpage
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'demo_1/index.html')
+    webpages_list = AccessRecord.objects.order_by('date')
+    print(webpages_list)
+    date_dict = {'access_records': webpages_list}
+    return render(request, 'demo_1/index.html', context=date_dict)
