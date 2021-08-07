c65a58fccc5671b7ee38217ca56e43921896d8b0
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-07 14:38:50 +0530

    add: added view for form rendering

--- basicAPI/demo_1/urls.py
+++ basicAPI/demo_1/urls.py
@@ -3,4 +3,5 @@ from demo_1 import views
 
 urlpatterns = [
     url('', views.index, name="index"),
+    url('form', views.form_name_view, name="form_view"),
 ]
--- basicAPI/demo_1/views.py
+++ basicAPI/demo_1/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from demo_1.models import AccessRecord, Topic, Webpage
+from . import forms
 
 # Create your views here.
 
@@ -8,3 +9,8 @@ def index(request):
     webpages_list = AccessRecord.objects.order_by('date')
     date_dict = {'access_records': webpages_list}
     return render(request, 'demo_1/index.html', context=date_dict)
+
+
+def form_name_view(request):
+    form = forms.FormName()
+    render(request, 'demo_1/forms/html', {'form': form})
