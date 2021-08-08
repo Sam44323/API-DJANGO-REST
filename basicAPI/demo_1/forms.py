72ae9f755fc1db88cf436f62d1191323b4f7d435
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-08 15:25:34 +0530

    add: added super form checker for all clean and emails

--- basicAPI/demo_1/forms.py
+++ basicAPI/demo_1/forms.py
@@ -9,3 +9,12 @@ class FormName(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[
                                  validators.MaxLengthValidator(0)])
+
+    def clean(self):
+        all_clean_data = super().clean()  # cleaning all the form data at once
+        email = all_clean_data['email']
+        vmail = all_clean_data['verifyemail']
+
+        if email != vmail:
+            raise forms.ValidationError(
+                'Make sure your email matches! try again.')
