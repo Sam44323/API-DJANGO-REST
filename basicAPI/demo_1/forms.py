ce260a32a3281a95c5a814a5d4abe1f7273c079e
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-08 15:15:45 +0530

    add: added a verify-email input

--- basicAPI/demo_1/forms.py
+++ basicAPI/demo_1/forms.py
@@ -2,14 +2,10 @@ from django import forms
 from django.core import validators
 
 
-def check_for_z(value):
-    if value[0].lower() != 'z':
-        raise forms.ValidationError("NAME NEEDS TO START WITH Z!")
-
-
 class FormName(forms.Form):
-    name = forms.CharField(validators=[check_for_z])
+    name = forms.CharField()
     email = forms.EmailField()
+    verifyemail = forms.EmailField(label="Enter your email again:")
     text = forms.CharField(widget=forms.Textarea)
     botcatcher = forms.CharField(required=False, widget=forms.HiddenInput, validators=[
                                  validators.MaxLengthValidator(0)])
